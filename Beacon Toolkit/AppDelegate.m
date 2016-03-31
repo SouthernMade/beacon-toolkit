288c2e76054f570f1fbbfd9ad91be888d146191f
Justin Ramos <justin@southernmade.co>
2016-03-31 22:39:42 +0000

    move default UUID to settings bundle

--- Beacon Toolkit/AppDelegate.m
+++ Beacon Toolkit/AppDelegate.m
@@ -16,10 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Register the preference defaults early.
-    NSDictionary *appDefaults = [NSDictionary dictionaryWithObject:@"E20A39F4-73F5-4BC4-A12F-17D1AD07A961" forKey:@"beaconUUID"];
-    [[NSUserDefaults standardUserDefaults] registerDefaults:appDefaults];
-
     return YES;
 }
 
